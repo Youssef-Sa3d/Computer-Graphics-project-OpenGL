9f7ecfcff5323eefb26234a1aaf1d1cfc50b8404
Youssef Saad <99206665+Youssef-Sa3d@users.noreply.github.com>
2023-01-03 00:59:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ void init(){
 
 }
 //------------------------------  reshapeFunc  ---------------------------------
-//commit 7
+//commit 8
 void reshapeFunc (int w, int h)
 {
     glViewport(0,0,(GLsizei)w,(GLsizei)h);
